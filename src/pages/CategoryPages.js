a0812c44b66a5d2aa565e26acaacda725daf4908
Siddharth <siddharthgohil07@gmail.com>
2024-01-15 03:11:37 +0530

    add category page

---
+++ src/pages/CategoryPages.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import EventGrid from '../components/EventGrid'
+import eventService from '../services/eventData';
+
+const CategoryPages = () => {
+
+  const urlParams = new URLSearchParams(window.location.search);
+
+  const category = urlParams.get('category');
+
+  const [eventList, setEventList] = useState([]);
+  const headerText = "Top " + category + " events for you";
+
+  useEffect(() => {
+    // setLoading(true);
+
+    const fetchData = async () => {
+      try {
+        // Simulate a delay of 1 second
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        const response = await eventService.getEventByCategory(category);
+        setEventList(response.data);
+        // setTotalEvents(response.data.length);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        // setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
+  return (
+    <div>
+      {/* <h1>Header section pending for category pages</h1> */}
+      <EventGrid eventList={eventList} headerText={headerText} />
+    </div>
+  )
+}
+
+export default CategoryPages
